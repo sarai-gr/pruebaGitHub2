f53a32e09f991a2ff081212be7841f964804804b
SaraiG <sarairivera005@gmail.com>
2024-09-30 19:03:14 -0600

    Cambio2

--- app/src/main/java/com/example/pruebagithub/MainActivity.kt
+++ app/src/main/java/com/example/pruebagithub/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             PruebaGitHubTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Tito",
+                        name = "LAKs",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
